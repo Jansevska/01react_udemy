d16e2e581a45f2ce1742ea0568e7d2660e51e9b7
Emili <ejanchevis@gmail.com>
2024-02-14 17:11:50 -1000

    added dynamic content to Header paragrath

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
+  {/* curly braces makes dynamic content, reactDescriptions is a list of content you want to be dynamic
+  in square brakets you can input the function to make the array content dynamic with the max of 2 in this case 
+  to make it more readable created a description variable*/}
+
   return (
     <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
