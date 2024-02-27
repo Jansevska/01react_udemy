50c93535bcae47106315d128412ac648462576a6
Emili <ejanchevis@gmail.com>
2024-02-26 15:58:22 -1000

    added key to CoreConcept dynamic list content

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ if (selectedTopic) {
         <section id="core-concepts">
         <h2>Core Concepts</h2>
         <ul>
-          {CORE_CONCEPTS.map((conceptItem) => <CoreConcept {...conceptItem} />)}
+          {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
         </ul>
         </section>
         <section id="examples">
