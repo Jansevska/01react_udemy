edb055d21a60170e9322b182cc5fa4e04f9b2297
Emili <ejanchevis@gmail.com>
2024-02-26 14:07:01 -1000

    added useState to App() so UI changes as TabButtons are clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
+
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 
 function App() {
-  function handleSelect() {
-    console.log('Hello World - selected!');
+  const [ selectedTopic, setSelectedTopic ] = useState('components');
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    // console.log(selectedButton);
+    // tabContent = selectedButton; after setting setSelectedTopic(selectedButton) go down the page and change 
+    // {tabContent} to {selectedTopic}
+    setSelectedTopic(selectedButton);
+    // console.log(tabContent);
+    console.log(selectedTopic);
 }
 
 
@@ -30,13 +41,21 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {/* Dynamic Content for TabButtons */}
-          
+          {/* Dynamic Content for TabButtons = {tabContent}*/}
+          {/* {tabContent} */}
+          {selectedTopic}
+          <div id="tab-content">
+            <h3>{ EXAMPLES[selectedTopic].title }</h3>
+            <p>{ EXAMPLES[selectedTopic].description }</p>
+            <pre>
+              <code>{ EXAMPLES[selectedTopic].code }</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
